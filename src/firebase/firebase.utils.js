5ab11290b74ece44e566e3a60e58487d8bd2101d
Roman Grubic <grubic.roman@gmail.com>
2020-07-07 14:53:00 +0100

    Integrating Firebase in app

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,32 @@
+// We need first one to allow firebase
+import firebase from 'firebase/app';
+// Next are the ones we wish to implement to our app
+// Firestore as database
+import 'firebase/firestore';
+// Auth for user authorization
+import 'firebase/auth';
+
+// Configuration for Firebase
+const config = {
+    apiKey: 'AIzaSyCh7Xv1HCF5A-HLUTc-2wcaJVL8WHP_rEw',
+    authDomain: 'shop-db-a9eed.firebaseapp.com',
+    databaseURL: 'https://shop-db-a9eed.firebaseio.com',
+    projectId: 'shop-db-a9eed',
+    storageBucket: 'shop-db-a9eed.appspot.com',
+    messagingSenderId: '134068313807',
+    appId: '1:134068313807:web:ae018b5ceac01842aba1ba',
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+// To apply the default browser preference language instead of explicitly setting it.
+firebase.auth().useDeviceLanguage();
+export const firestore = firebase.firestore();
+
+// Google sign-in https://firebase.google.com/docs/auth/web/google-signin
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
