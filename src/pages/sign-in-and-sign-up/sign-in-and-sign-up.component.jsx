9866771471583afc2c18d74302c16ac51043d045
Roman Grubic <grubic.roman@gmail.com>
2020-07-07 11:53:10 +0100

    Added SignIn component

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './sign-in-and-sign-up.styless.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUp = () => {
+    return (
+        <div>
+            <SignIn />
+        </div>
+    );
+};
+
+export default SignInAndSignUp;
