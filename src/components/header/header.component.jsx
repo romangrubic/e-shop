cfe014545500d0c56f8f003af8387c630e0caf79
Roman Grubic <grubic.roman@gmail.com>
2020-07-08 19:27:39 +0100

    Added redux to Header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,8 @@ import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+// It gives component access to Redux
+import { connect } from 'react-redux';
 
 const Header = ({ currentUser }) => (
     <div className='header'>
@@ -28,4 +30,9 @@ const Header = ({ currentUser }) => (
         </div>
     </div>
 );
-export default Header;
+
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
