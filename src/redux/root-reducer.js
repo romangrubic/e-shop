cce458dc00870b0a10a03cb03df06f2ed05b206e
Roman Grubic <grubic.roman@gmail.com>
2020-07-09 14:28:17 +0100

    created cart reducer for CartDropdown rendering

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,9 @@
 // Combining all reducers into a big one
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
     user: userReducer,
+    cart: cartReducer,
 });
