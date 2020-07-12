b667cebf4359ad31a87063a23e6327c052f7ec0f
Roman Grubic <grubic.roman@gmail.com>
2020-07-12 13:04:21 +0100

    Created decrement quantity  of item action

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,3 +15,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     // If item doesn't exist, it will push to array and set quantity to 1
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemToCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(
+            (cartItem) => cartItem.id !== cartItemToRemove.id
+        );
+    }
+    return cartItems.map((cartItem) =>
+        cartItem.id === cartItemToRemove.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+    );
+};
