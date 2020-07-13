3d93caca6d57bdb5ba8da1db787fa1691a321c72
Roman Grubic <grubic.roman@gmail.com>
2020-07-13 12:09:37 +0100

    Added collection item to collection page

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CollectionPreview from '../prewiev-collection/collection-preview.component';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selector';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => {
@@ -16,7 +16,7 @@ const CollectionsOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections,
+    collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
