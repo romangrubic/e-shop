6e986c969fa788675495f678d3f937f0a421a213
Roman Grubic <grubic.roman@gmail.com>
2020-07-29 14:37:32 +0100

    Changing component to class and to take new data from the reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import { Route } from 'react-router-dom';
 import Collection from '../collection/collection.component';
+import {
+    firestore,
+    convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
-const Shop = ({ match }) => {
-    return (
-        <div className='shop-page'>
-            <Route
-                exact
-                path={`${match.path}`}
-                component={CollectionsOverview}
-            />
-            <Route
-                path={`${match.path}/:collectionId`}
-                component={Collection}
-            />
-        </div>
-    );
-};
+class Shop extends Component {
+    unsubscribeFromSnapshot = null;
 
-export default Shop;
+    componentDidMount() {
+        const { updateCollections } = this.props;
+        const collectionRef = firestore.collection('collections');
+
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+            async (snapshot) => {
+                const collectionsMap = convertCollectionsSnapshotToMap(
+                    snapshot
+                );
+                updateCollections(collectionsMap);
+            }
+        );
+    }
+    render() {
+        const { match } = this.props;
+        return (
+            <div className='shop-page'>
+                <Route
+                    exact
+                    path={`${match.path}`}
+                    component={CollectionsOverview}
+                />
+                <Route
+                    path={`${match.path}/:collectionId`}
+                    component={Collection}
+                />
+            </div>
+        );
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    updateCollections: (collectionsMap) =>
+        dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(Shop);
