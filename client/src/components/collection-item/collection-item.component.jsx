b8f1533c4958cbc515c9fdcdb313a2517143fb0e
Roman Grubic <grubic.roman@gmail.com>
2020-08-10 15:11:43 +0100

    Transfering functions and implementing item modal component

--- client/src/components/collection-item/collection-item.component.jsx
+++ client/src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss';
-import { connect } from 'react-redux';
-import { addItem } from '../../redux/cart/cart.actions';
+import CollectionItemModal from '../collection-item-modal/collection-item-modal.component';
 
-const CollectionItem = ({ item, addItem }) => {
+const CollectionItem = ({ item }) => {
+    const [itemModalOpen, setItemModalOpen] = useState(false);
+
     const { name, price, imageUrl } = item;
 
     return (
@@ -17,14 +18,16 @@ const CollectionItem = ({ item, addItem }) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <CustomButton onClick={() => addItem(item)} inverted>
-                ADD TO CART
+            <CustomButton onClick={() => setItemModalOpen(true)} inverted>
+                Click to view!
             </CustomButton>
+            <CollectionItemModal
+                item={item}
+                open={itemModalOpen}
+                onClose={() => setItemModalOpen(false)}
+            />
         </div>
     );
 };
 
-const mapDispatchToProps = (dispatch) => ({
-    addItem: (item) => dispatch(addItem(item)),
-});
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default CollectionItem;
