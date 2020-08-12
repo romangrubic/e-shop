b5fbf7002c13a34831b01e23b0812560089ae3db
Roman Grubic <grubic.roman@gmail.com>
2020-08-12 13:46:45 +0100

    Creating SizeChoices component and connecting it to redux store

---
+++ client/src/components/size-choices/size-choices.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './size-choices.styles.scss';
+import { setSize } from '../../redux/cart/cart.actions';
+import { connect } from 'react-redux';
+
+const SizeChoices = ({ category, setSize }) => {
+    let sizeChoices = ['Small', 'Medium', 'Large'];
+
+    if (category === 'Sneakers') sizeChoices = [6, 6.5, 7, 7.5, 8, 8.5, 9];
+    if (category === 'Hats') sizeChoices = ['Fits all'];
+    if (category === 'Womens')
+        sizeChoices = ['S (6-8)', 'M (10-12)', 'L (14-16)'];
+    if (category === 'Mens')
+        sizeChoices = ['S (36)', 'M (38)', 'L (40)', 'XL (42)'];
+
+    return (
+        <div className='center-buttons'>
+            {sizeChoices.map((size) => (
+                <button
+                    key={size}
+                    className='choice-button'
+                    onClick={() => setSize(`${size}`)}
+                >
+                    {size}
+                </button>
+            ))}
+        </div>
+    );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    setSize: (size) => dispatch(setSize(size)),
+});
+
+export default connect(null, mapDispatchToProps)(SizeChoices);
