00f427d1e795ac7098503a1759721abf0506d544
Roman Grubic <grubic.roman@gmail.com>
2020-08-14 13:27:18 +0100

    Added contact details to contact page.

--- client/src/pages/contact/contact.component.jsx
+++ client/src/pages/contact/contact.component.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import './contact.styles.scss';
 
 const Contact = () => {
     return (
-        <div>
+        <div className='contact-page'>
             <h1>Hello!</h1>
-            <h2>If you like my shop, why don't you contact me?</h2>
+            <h2>
+                My name is Roman Grubic and i'm developer of this
+                web-application.
+            </h2>
+            <p className='description'>
+                I enjoy working with ReactJS and Redux and i'm currently
+                learning MERN stack. I love to develop web-sites from
+                presentational apps (portfolio) to fully functional e-commerce
+                shop (like this one!). And if you are looking for a web-site for
+                yourself or for your company, don't hesitate to contact me!
+                <br /> <br /> I named this shop after Star Trek: Deep Space Nine
+                character Garak, who in the show has a tailor shop named
+                "Garak's Clothiers".
+            </p>
+            <hr />
+            <h3>
+                Github: {''}
+                <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://github.com/romangrubic'
+                >
+                    https://github.com/romangrubic
+                </a>
+            </h3>
+            <h3>
+                LinkedIn: {''}
+                <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://www.linkedin.com/in/roman-grubic/'
+                >
+                    https://www.linkedin.com/in/roman-grubic/
+                </a>
+            </h3>
+            <h3>E-mail: grubic.roman@gmail.com</h3>
         </div>
     );
 };
